b4977c3f376e1b5bf512f7d74c12b29074b81baa
Peter Xiao <phx@andrew.cmu.edu>
2012-11-02 20:36:19 -0400

    Uploading notes now works.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,7 @@
 class Note < ActiveRecord::Base
-  attr_accessible :course_id, :data, :filename, :rating, :title, :type, :user_id
+  attr_accessible :course_id, :filename, :rating, :title, :user_id
   # Relationships
   belongs_to :course
+  
+  mount_uploader :filename, FileUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121102230734) do
+ActiveRecord::Schema.define(:version => 20121103002009) do
 
   create_table "courses", :force => true do |t|
     t.string   "name"
@@ -26,8 +26,6 @@ ActiveRecord::Schema.define(:version => 20121102230734) do
     t.integer  "course_id"
     t.integer  "user_id"
     t.integer  "rating"
-    t.string   "file_type"
-    t.binary   "data"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
